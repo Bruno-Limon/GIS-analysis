e23d6e9281727cb38ef2cebcc3f1d5d746c552ce
Bruno-Limon <brunolimon@hotmail.com>
2023-05-06 16:10:51 +0200

    update

--- Src/data-cleaning.py
+++ Src/data-cleaning.py
@@ -3,17 +3,8 @@ import pandas as pd
 
 air_quality_index = 'https://raw.githubusercontent.com/Bruno-Limon/air-quality-analysis/main/Data/AQI-2016.csv'
 df_aqi = pd.read_csv(air_quality_index)
-df_aqi.info(verbose = 1)
-print(df_aqi)
-
 df_aqi.insert(0, 'Datetime', pd.to_datetime(df_aqi['Fecha'] + ' ' + df_aqi['Hora']))
-# df_aqi.insert(0, 'Datetime', pd.to_datetime(df_aqi['Fecha'] + ' ' + df_aqi['Hora'], format = 'mixed'))
 df_aqi.drop(['Fecha', 'Hora'], axis = 1, inplace = True)
-
-# Looking at unique values and time span
-print('Distinct monitoring stations:', df_aqi['CASETA '].unique())
-print('Earliest date:', df_aqi['Datetime'].min())
-print('Latest date:', df_aqi['Datetime'].max())
 
 # Mapping the 3 letter code of monitoring stations to their full name, for clarity
 dict_caseta = {'ATM': 'Atemajac',
@@ -30,19 +21,17 @@ dict_caseta = {'ATM': 'Atemajac',
 df_aqi["Caseta"] = df_aqi['CASETA '].map(dict_caseta)
 df_aqi.drop(['CASETA '], axis = 1, inplace = True)
 
+# Dropping unncessary features or those with over 50% missing values
+to_drop = ['PM2.5 ', 'Radiacion solar', 'Indice UV', 'Direccion de vientos ']
+df_aqi.drop(to_drop, axis=1, inplace=True)
+df_aqi.rename(columns={'Humedad relativa ': 'Humidity',
+                       'Velocidad de viento ': 'Wind velocity',
+                       'Caseta': 'Station',
+                       'Temperatura': 'Temperature'}, inplace=True)
+
 list_monitor_station = list(df_aqi['Station'].unique())
 list_columns = list(df_aqi.columns.values)[1:9]
 
-# Creating list containing null values filtered by the monitoring station
-list_nulls = []
-for i, station in enumerate(list_monitor_station):
-    list_nulls.append([])
-    for j in range(len(list_columns)):
-        list_nulls[i].append(
-            df_aqi.loc[df_aqi['Station'] == station].iloc[:, j+1].isnull().sum())
-
-# Putting the lists into a dataframe
-df_aqi_nulls = pd.DataFrame(np.column_stack([list_nulls[i] for i in range(len(list_monitor_station))]),
-                            columns=list_monitor_station,
-                            index=list_columns)
-print(df_aqi_nulls.T)
+# Removing the monitoring stations with features consisting entirely of missing values as well as the anomaly in SO2
+df_aqi = df_aqi[df_aqi.Station.str.contains('Santa Fe|Centro') == False]
+df_aqi = df_aqi.drop([43816, 14554])
